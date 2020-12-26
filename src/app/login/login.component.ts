4a9c8602790622bc2e7579e1dffe7dcb7d918f60
Rosemary Titus <rosemarytitus1998@gmail.com>
2020-12-26 11:53:28 -0800

    firstcommit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { utf8Encode } from '@angular/compiler/src/util';
+import { Component, OnInit } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  
+  constructor() { }
+  @Output() newItemEvent = new EventEmitter<string>();
+  
+login(l,p)
+{
+  this.newItemEvent.emit(l.value);
+console.log(l.value);
+console.log(p.value);
+}
+  ngOnInit(): void {
+  }
+
+}
